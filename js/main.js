f55b5c38f80152206ca49348531a2470daf9f919
fofoy <florian.quiblier@gmail.com>
2012-12-14 13:53:12 +0100

    Ajout zoom + temp infos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var updateLayout = function() {
+  if (window.innerWidth != currentWidth) {
+    currentWidth = window.innerWidth;
+    var orient = (currentWidth == 320) ? "profile" : "landscape";
+    document.body.setAttribute("orient", orient);
+    window.scrollTo(0, 1);
+  }
+};
+
+iPhone.DomLoad(updateLayout);
+setInterval(updateLayout, 500);
+console.log('ok');
