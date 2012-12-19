22958afd3f5e6e90d8ea4305c70aaf2856f45474
guillaumemarq <contact@guillaume-marq.com>
2012-12-19 18:06:26 +0100

    js budget css

--- desktop/js/fonctions.js
+++ desktop/js/fonctions.js
@@ -3,9 +3,3 @@ function showInfos(){
 	supp.className = (supp.className == 'on') ? '' : 'on';
 	return false;
 }
-
-function validstep(){
-	var done=document.getElementById('etape2done');
-	done.className = (done.className == 'on') ? '' : 'on';
-	return false;
-}
