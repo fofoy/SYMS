f005bbf4b206d010f4152e8e3850fa9c643b1375
guillaumemarq <contact@guillaume-marq.com>
2012-12-19 16:45:38 +0100

    sd

--- desktop/js/fonctions.js
+++ desktop/js/fonctions.js
@@ -3,3 +3,9 @@ function showInfos(){
 	supp.className = (supp.className == 'on') ? '' : 'on';
 	return false;
 }
+
+function validstep(){
+	var done=document.getElementById('etape2done');
+	done.className = (done.className == 'on') ? '' : 'on';
+	return false;
+}
