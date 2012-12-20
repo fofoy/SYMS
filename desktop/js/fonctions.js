33c96a03085696e9b7b3f79e79b23260628524c6
guillaumemarq <contact@guillaume-marq.com>
2012-12-20 10:27:27 +0100

    anim js

--- desktop/js/fonctions.js
+++ desktop/js/fonctions.js
@@ -3,3 +3,10 @@ function showInfosListe(){
 	suppListe.className = (suppListe.className == 'on') ? '' : 'on';
 	return false;
 }
+
+
+function showInfosBudget(){
+	var suppBudget=document.getElementById('infosSup');
+	suppListe.className = (suppListe.className == 'on') ? '' : 'on';
+	return false;
+}
